43561c9c143dfada7b80acc610f0632ad80f0a3b
inchis <inchis@users.noreply.github.com>
2017-03-21 06:53:19 +0200

    Update py-django-connector.py

--- py-django-connector.py
+++ py-django-connector.py
@@ -1,10 +1,3 @@
-from views import process_data, process_data_2
-
- url(r'^odoo/rest/verify/$', process_data, name="Odoo-App-Connector"),
- url(r'^odoo/rest/contactupdate/$', process_data_2, name="Odoo-App-Connector-2"),
-
-
-
 import sys
 from django.utils import simplejson
 import re
